c3c2cb15f215f8d46f0355ca764846ab4f73ad5b
Franco <franco.driansetti@gmail.com>
2024-04-12 10:04:56 +0200

    Initial Commit

---
+++ chatui-ios/chatui_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatui_iosApp.swift
+//  chatui-ios
+//
+//  Created by Franco Driansetti on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct chatui_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
